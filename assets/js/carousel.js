adac3be34acf405aa91e967f4879c6b430d6b83a
Jibril Mohamod <jazimjibril@gmail.com>
2022-06-18 17:28:55 +0300

    Dinal 1

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,5 +1,21 @@
-window.addEventListener('load', function(){
-  new Glider(document.querySelector('.glider'), {
-    setting-name: setting-value
-  })
+const swiper = new Swiper(".swiper", {
+ // Optional parameters
+ direction: "vertical",
+ loop: true,
+
+ // If we need pagination
+ pagination: {
+  el: ".swiper-pagination",
+ },
+
+ // Navigation arrows
+ navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+ },
+
+ // And if we need scrollbar
+ scrollbar: {
+  el: ".swiper-scrollbar",
+ },
 })
